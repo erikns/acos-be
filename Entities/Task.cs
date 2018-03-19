04db51bb8d6ecc5bbae89e23ac4e800a6ddc1731
Erik Sørensen <eriksorensen88@gmail.com>
2018-03-20 00:20:07 +0100

    Connect task to user

--- Entities/Task.cs
+++ Entities/Task.cs
@@ -8,6 +8,6 @@ namespace ACOS_be.Entities
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        //public User User { get; set; }
+        public User User { get; set; }
     }
 }
---
+++ Migrations/20180319231732_ConnectToUser.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ACOSbe.Migrations
+{
+    public partial class ConnectToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Task",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Task_UserId",
+                table: "Task",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Task_User_UserId",
+                table: "Task",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Task_User_UserId",
+                table: "Task");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Task_UserId",
+                table: "Task");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Task");
+        }
+    }
+}
